e76db6191ba164eef168127cc99d4a1c1d9c7794
ASchopenhaur <alexhernandez800@hotmail.com>
2017-02-21 22:28:03 -0600

    set up cors and user routes

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/users');
+
+// Port Number
 const port = 3000;
 
+// CORS Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+// Start Server
 app.listen(port, function(){
     console.log('Server started on port ' + port);
 });
